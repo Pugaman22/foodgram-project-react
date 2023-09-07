3f999da69f7c621d87b4407213295b5c2d094ba1
Andrew Pugach <andrewpugach17@gmail.com>
2023-09-07 19:24:52 +0300

    First commit

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from users.models import Subscription
 
-# Register your models here.
+User = get_user_model()
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'username', 'email', 'first_name', 'last_name')
+    list_filter = ('username', 'first_name', 'last_name')
+    search_fields = ('username', 'first_name', 'last_name')
+    ordering = ('username')
+
+
+@admin.register(Subscription)
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'author', 'user',)
+    list_filter = ('author', 'user',)
+    search_fields = ('author', 'user',)
--- backend/users/models.py
+++ backend/users/models.py
@@ -5,18 +5,18 @@ from django.core.validators import RegexValidator
 
 class User(AbstractUser):
     email = models.EmailField(
-        max_length=150,
+        max_length=254,
         unique=True,
         verbose_name='E-mail',
     )
     username = models.CharField(
-        max_length=100,
+        max_length=150,
         unique=True,
         verbose_name='Login',
         validators=[
             RegexValidator(
-                regex=r'^[\w-]*$',
-                message='Use characters from this list [a-zA-Z0-9_-]',
+                regex=r'^[\w.@+-]+\z',
+                message='Use characters from this list [a-zA-Z0-9_-+.]',
             ),
         ]
     )
@@ -31,8 +31,39 @@ class User(AbstractUser):
     )
 
     class Meta:
-        ordering = ('-id',)
+        ordering = ('username',)
         verbose_name_plural = 'Users'
+        constraints = [
+            models.UniqueConstraint(
+                fields=['username', 'email'],
+                name='unique_username_and_email'
+            )
+        ]
 
     def __str__(self):
         return self.username
+
+
+class Subscription(models.Model):
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='recipe_author'
+    )
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='subscriber'
+    )
+
+    class Meta:
+        verbose_name_plural = 'Subscriptions'
+        constraints = [
+            models.UniqueConstraint(
+                fields=['author', 'user'],
+                name='unique_author_and_subscriber'
+            )
+        ]
+
+    def __str__(self):
+        return f'{self.user} subscribed on {self.author}'
