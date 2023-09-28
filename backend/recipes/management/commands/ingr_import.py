2a9ff28c43434dbf80a1118d4db9c7d2b6f5b157
Andrew Pugach <andrewpugach17@gmail.com>
2023-09-28 15:45:04 +0300

    Docker 4

--- backend/recipes/management/commands/ingr_import.py
+++ backend/recipes/management/commands/ingr_import.py
@@ -1,21 +1,4 @@
-# import json
 
-# from django.core.management.base import BaseCommand
-# from recipes.models import Ingredient
-
-
-# class Command(BaseCommand):
-
-#     def handle(self, *args, **options):
-#         with open('../data/ingredients.json', 'rb') as f:
-#             data = json.load(f)
-#             print("loaded:", len(data))
-#             for index, item in enumerate(data):
-#                 print(f'add {index}')
-#                 Ingredient.objects.create(
-#                     name=item['name'],
-#                     measurement_unit=item['measurement_unit']
-#                 )
 
 import csv
 
@@ -43,5 +26,5 @@ class Command(BaseCommand):
                 model.objects.bulk_create(model(**data) for data in reader)
 
         self.stdout.write(self.style.SUCCESS(
-            'Data id loaded')
+            'Data is loaded')
         )
